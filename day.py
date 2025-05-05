cbfde76adcbb5749115c1fbacdbf7a29c01799a3
adwaitgamer123 <deepthianoop123@gmail.com>
2025-05-05 17:18:52 +0300

    hi maam

---
+++ day.py
@@ -0,0 +1,36 @@
+print(" Welcome to my app")
+birthday1 = input("When is Adwait's Birthday?\n")
+if birthday1 == "23": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall")
+
+    birthday2 = input("When is Adwait's Mother's Birthday?\n")
+if birthday2 == "3": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall")  
+
+    birthday3 = input("When is Adwait's Father's Birthday?\n") 
+if birthday3 == "30": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall") 
+
+    birthday4 = input("When is Adwait's Brother's Birthday?\n") 
+if birthday4 == "30": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall")   
+
+    birthday5 = input("When is Adwait's Grandfather's Birthday?\n") 
+if birthday5 == "18": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall")
+
+    birthday6 = input("When is Adwait's Grandmother's Birthday?\n") 
+if birthday6 == "13": 
+    print("Correct,You Remeber it") 
+else :
+    print("Incorrect,Try to recall")      
